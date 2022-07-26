89c1ba5c2275ee31b2a1b55eab206ed0b235dade
jabrayare <jabra5980@gmail.com>
2022-07-26 14:32:09 -0400

    updated the home page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p> Hello World(Made some changes here and pushed to azure deveops repo!) </p>"
+  return "<p> Hello World(Made some changes here and pushed to azure deveops repo!) Tryin to push to github again! </p>"
 
 if __name__ == "__main__":
   app.run()
